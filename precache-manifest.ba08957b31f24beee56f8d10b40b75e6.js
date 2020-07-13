f044921d3e2a889f7de4bbd227524ed33fc1cb42
y0092888 <ju-horn@web.de>
2020-07-13 14:33:45 +0200

    init

---
+++ precache-manifest.ba08957b31f24beee56f8d10b40b75e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9fd8e0613cface0d3e5428df11ccae",
+    "url": "/hackerconsole/index.html"
+  },
+  {
+    "revision": "19d2098b79368c78ed07",
+    "url": "/hackerconsole/static/css/main.e04cecf0.chunk.css"
+  },
+  {
+    "revision": "836a2907e3ea4a9da1ea",
+    "url": "/hackerconsole/static/js/2.943832ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hackerconsole/static/js/2.943832ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d2098b79368c78ed07",
+    "url": "/hackerconsole/static/js/main.bd20ca92.chunk.js"
+  },
+  {
+    "revision": "b2cac71dbb0f3168a263",
+    "url": "/hackerconsole/static/js/runtime-main.628c27a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hackerconsole/static/media/logo.5d5d9eef.svg"
+  }
+]);
